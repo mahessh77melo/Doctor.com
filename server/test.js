7903cf93ab4c28acacc8e571b73246527b99c003
mahessh77melo <magesh.lbj23@gmail.com>
2020-10-19 15:13:50 +0530

    test.js change

--- server/test.js
+++ server/test.js
@@ -1,7 +1,7 @@
 // run this file to get all the entries in the database including the ones which you inserted recently  (if any)
 // node test.js (in your cmd)
 
-/* const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const Application = require("./db_schema");
 mongoose.connect(
 	"mongodb+srv://magesh:mongo123@mycluster.rhps1.mongodb.net/doctor_db?retryWrites=true&w=majority",
@@ -13,8 +13,8 @@ Application.find((err, res) => {
 		console.log(res);
 	}
 });
- */
-var nodemailer = require("nodemailer");
+
+/* var nodemailer = require("nodemailer");
 
 var transporter = nodemailer.createTransport({
 	service: "gmail",
@@ -38,3 +38,4 @@ transporter.sendMail(mailOptions, function (error, info) {
 		console.log("Email sent: " + info.response);
 	}
 });
+ */

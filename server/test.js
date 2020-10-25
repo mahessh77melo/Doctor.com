76326be11707afdf3fbcacbd3f70a9cc853ff216
mahessh77melo <magesh.lbj23@gmail.com>
2020-10-25 19:53:50 +0530

    uninstalled unwanted dependencies

--- server/test.js
+++ server/test.js
@@ -3,39 +3,11 @@
 
 const mongoose = require("mongoose");
 const Application = require("./db_schema");
-mongoose.connect(
-	"mongodb+srv://magesh:mongo123@mycluster.rhps1.mongodb.net/doctor_db?retryWrites=true&w=majority",
-	{ useNewUrlParser: true, useUnifiedTopology: true }
-);
+const { dbAtlas } = require("./mongo_data");
+mongoose.connect(dbAtlas, { useNewUrlParser: true, useUnifiedTopology: true });
 
 Application.find((err, res) => {
 	if (!err) {
 		console.log(res);
 	}
 });
-
-/* var nodemailer = require("nodemailer");
-
-var transporter = nodemailer.createTransport({
-	service: "gmail",
-	auth: {
-		user: "magesh.lbj23@gmail.com",
-		pass: "mag26esh",
-	},
-});
-
-var mailOptions = {
-	from: "harish30curry@gmail.com",
-	to: "stunner.magesh@gmail.com",
-	subject: "Sending Email using Node.js",
-	text: "That was easy!",
-};
-
-transporter.sendMail(mailOptions, function (error, info) {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log("Email sent: " + info.response);
-	}
-});
- */
